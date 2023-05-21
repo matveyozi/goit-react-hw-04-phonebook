1782f4489d6cd0849faff8de3abb1571ddf81af0
Matviy <antipin.m9971@gmail.com>
2023-05-21 23:03:26 +0300

    delete component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList'
